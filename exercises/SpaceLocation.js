15c716755dfbcef9929240046c23a1283d38a188
Carnahan Lovewell <carnahan.lovewell@gmail.com>
2022-08-15 16:48:19 -0500

    Solved exercises.

---
+++ exercises/SpaceLocation.js
@@ -0,0 +1,15 @@
+"use strict";
+exports.__esModule = true;
+exports.SpaceLocation = void 0;
+// Paste in the provided code here:
+var SpaceLocation = /** @class */ (function () {
+    function SpaceLocation(name, kilometersAway) {
+        this.name = name;
+        this.kilometersAway = kilometersAway;
+    }
+    SpaceLocation.prototype.printDaysToLocation = function (location) {
+        console.log("".concat(this.name, " will take ").concat(this.getDaysToLocation(location.kilometersAway), "\n         to reach ").concat(location.name, "."));
+    };
+    return SpaceLocation;
+}());
+exports.SpaceLocation = SpaceLocation;
--- exercises/SpaceLocation.ts
+++ exercises/SpaceLocation.ts
@@ -1,1 +1,15 @@
 // Paste in the provided code here:
+export class SpaceLocation {
+    kilometersAway: number;
+    name: string;
+ 
+    constructor(name: string, kilometersAway: number) {
+       this.name = name;
+       this.kilometersAway = kilometersAway;
+    }
+
+    printDaysToLocation(location: SpaceLocation): void {
+        console.log(`${this.name} will take ${this.getDaysToLocation(location.kilometersAway)}
+         to reach ${location.name}.`);
+    }
+ }
